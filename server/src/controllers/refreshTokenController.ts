5c2fea09acee2a95cd9b7e0a1b353475ddd357cd
VitthalGund <vitthalgund30@gmail.com>
2023-11-29 11:30:51 +0530

    bug fixes is token verification jwt Token type used to add type safety

--- server/src/controllers/refreshTokenController.ts
+++ server/src/controllers/refreshTokenController.ts
@@ -1,6 +1,7 @@
 import { User } from "../models/User.js";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { JwtPayload } from "../type/jwtToken.js";
 
 // if user jwt token expires then using refresh token we can verify the user
 export const handleRefreshToken = async (req: Request, res: Response) => {
@@ -12,30 +13,30 @@ export const handleRefreshToken = async (req: Request, res: Response) => {
   // if user not found
   if (!foundUser) return res.sendStatus(403); //Forbidden
   // evaluate jwt to assign the new jwt token to user
-  jwt.verify(
+  const response: JwtPayload = jwt.verify(
     refreshToken,
-    process.env.REFRESH_TOKEN_SECRET!,
-    (err, decoded) => {
-      if (err || foundUser.username !== decoded.username)
-        return res.sendStatus(403);
-      const roles = Object.values(foundUser.roles);
-      const accessToken = jwt.sign(
-        {
-          UserInfo: {
-            username: decoded.username,
-            email: decoded.email,
-            roles: roles,
-          },
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "72h" }
-      );
-      res.json({
-        roles,
-        accessToken,
-        email: foundUser.email,
-        username: foundUser.username,
-      });
-    }
+    process.env.REFRESH_TOKEN_SECRET!
+  ) as JwtPayload;
+
+  if (foundUser.username !== response.UserInfo.username) {
+    return res.sendStatus(403);
+  }
+  const roles = Object.values(foundUser.roles!);
+  const accessToken = jwt.sign(
+    {
+      UserInfo: {
+        username: response.UserInfo.username,
+        email: response.UserInfo.email,
+        roles: roles,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET!,
+    { expiresIn: "72h" }
   );
+  res.json({
+    roles,
+    accessToken,
+    email: foundUser.email,
+    username: foundUser.username,
+  });
 };
